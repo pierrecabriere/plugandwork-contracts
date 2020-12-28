2948f893aa86fa72bd16a452a5773bc0ec211b72
Pierre Cabrière <hello@pierrecabriere.fr>
2020-12-28 09:48:40 +0100

    update dependencies & toolkit integration

--- src/index.tsx
+++ src/index.tsx
@@ -5,4 +5,13 @@ import ExampleApp from './app';
 export default class extends PlugandworkApp {
   static component = ExampleApp;
   static settings = settingsJSON;
+  static icon = require('../public/logo@128.png');
+
+  static async onInstall(view: PawModel) {
+    console.log("app installed with view :", view.id);
+  }
+
+  static async onUninstall(view: PawModel) {
+    console.log("app uninstalled");
+  }
 }
