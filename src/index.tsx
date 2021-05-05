e29235afd4cc5e266a024a2a61f65d930f261be2
Pierre Cabrière <hello@pierrecabriere.fr>
2021-05-05 10:24:31 +0200

    update dependencies

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,16 @@
 import { PlugandworkApp } from 'plugandwork-toolkit';
 import settingsJSON from "../settings.json";
 import App from './app';
+import PawModel from 'plugandwork-toolkit/dist/models/PawModel';
 
 export default class extends PlugandworkApp {
   static component = App;
   static settings = settingsJSON;
   static icon = require('../public/logo@128.png');
+
+  static handleDoc(doc: PawModel, view: PawModel) {
+    return false;
+  }
 
   static async onInstall(view: any) {
     console.log("app installed with view :", view.id);
